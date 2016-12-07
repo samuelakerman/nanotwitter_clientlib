4737d7a63e47a8e46a8d867447f6f4ce7448c587
Samuel Akerman <samsk66@gmail.com>
2016-12-07 18:58:00 -0500

    Update clientlib_test.rb
    Created unit test for clientlibt

--- clientlib_test.rb
+++ clientlib_test.rb
@@ -1,28 +1,56 @@
 require_relative 'clientlib'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'webrat/core/matchers'
+require "byebug"
+require_relative Dir.pwd+'/clientlib.rb'
 
-client = NanoTwitter.new 
-#client.init "http://127.0.0.1:4567"
+class TestClientLib < Minitest::Test
 
-#Specify URL were web server is
-client.init "http://nanotwitter-xx.herokuapp.com/"
+	def setup
+		@client = NanoTwitter.new 
+		@response_init = @client.init "http://nanotwitter-xx.herokuapp.com/"
+		@response_login = @client.login("Dan","Lt1j3sGi7")
+	end
 
-#Login as user Dan
-client_dan = client.login("Dan","096iL3856")
+	def test_init
+		assert_equal(1,@response_init)	
+	end
 
-#Fetch my followers
-client.my_followers
+	def test_login
+		assert_equal(1,@response_login)
+	end
 
-#Fetch the users I am following
-client.my_followings
+	def test_followers
+		#Fetch my followers
+		response_followers = @client.my_followers
+		assert_operator response_followers.count, :>=,9
+	end
 
-#Fetch my timeline
-client.my_timeline 50
+	def test_followings
+		#Fetch the users I am following
+		response_followings = @client.my_followings
+		assert_operator response_followings.count, :>=,2
+	end
 
-#Post tweet
-client.post_tweet "This is a test tweet for NanoTwitter" 
+	def test_timeline
+		response_timeline = @client.my_timeline 50
+		assert_operator response_timeline.count, :>=,19
+	end
 
-#Logout
-client.logout
+	def test_post_tweet
+		#Post tweet
+		response_post = @client.post_tweet "This is a test tweet for NanoTwitter" 
+		assert_equal(1,response_post)
+	end
 
-#Error. Not logged in
-client.logout
+	def test_follow_user
+		response_follow = @client.follow_user 8
+		assert_equal(1,response_follow)
+	end
+
+	def test_logout
+		response_logout = @client.logout
+		assert_equal(1,response_logout)
+	end
+end
